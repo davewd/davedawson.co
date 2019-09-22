e2864c1d49b54c32f523f756bb1b98171d7aaa0c
davewd <davewd@me.com>
2019-09-22 12:22:47 +1000

    Update site to be custom built

---
+++ site/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DaveDawson.co</h1>
+        <p>
+          A place holder for great things....
+        </p>
+        <h5>Sydney based Quantiative Developer, Leader, Mentor, Do-er</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
