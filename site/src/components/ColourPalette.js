69eada5a38a09712673b7815fe3f410999762f44
davewd <davewd@me.com>
2020-04-15 21:21:25 +1000

    Ensure tab can round robin the themes

--- site/src/components/ColourPalette.js
+++ site/src/components/ColourPalette.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPalette} from '@fortawesome/free-solid-svg-icons'
 import styled from 'styled-components'
+import { useTheme } from '../styling/ThemeContext';
+function ColourPalette (props){
 
-class ColourPalette extends React.Component{
-
-    render(){
-        return(
-            <div id="url-assets" style={{align: "right", width: "100%"}}>
-                <ul>
-                    <li><a onClick={() => {console.log("Toggle")}}><FontAwesomeIcon icon={faPalette} /></a></li>
-                </ul>
-            </div>
-        )
-    }
+    const themeToggle = useTheme();
+    
+    return(
+        <div id="url-assets" style={{align: "right", width: "100%"}}>
+            <ul>
+                <li><a onClick={() => themeToggle.toggle()}><FontAwesomeIcon icon={faPalette} /></a></li>
+            </ul>
+        </div>
+    )
 }
 
 
