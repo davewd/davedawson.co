5405281c648440c8aa832d76fbb357037de4d938
davewd <davewd@me.com>
2020-04-18 00:20:03 +1000

    imports

--- site/src/components/index.js
+++ site/src/components/index.js
@@ -2,3 +2,4 @@ export {default as NavHeader} from './NavHeader';
 export {default as OnlineIdentities} from './OnlineIdentities'
 export {default as ColourPalette} from './ColourPalette'
 export {default as Landing} from './Landing'
+export {default as Footer} from './Footer'
