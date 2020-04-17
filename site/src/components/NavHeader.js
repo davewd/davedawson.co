a65a60dddbe3d12057c733b09277899b0d1c8d64
davewd <davewd@me.com>
2020-04-17 22:52:42 +1000

    Finally cleared out react-bootstrap

--- site/src/components/NavHeader.js
+++ site/src/components/NavHeader.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import OnlineIdentities from './OnlineIdentities'
 import ColourPalette from './ColourPalette'
+import styled from 'styled-components'
+import { textColor, backgroundColor } from '../styling/theme'
 
+const NavWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+   
+div{ 
+  display: inline;
+  background: ${backgroundColor};
+  padding: 10px 20px 10px 20px;
+ 
+}
+ul {
+  list-style-type: none;
+  display: inline;  
+  padding: 0px 0px 0px 0px; 
+}
+
+li {
+  list-style-type: none;
+  display: inline;  
+  padding: 0px 20px 0px 20px; 
+}
+
+a {
+    color: ${textColor}
+    }
+`
 
 class NavHeader extends React.Component {
     render(){
         return(
-          <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">DD</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#overview">Overview</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#blog">Blog</Nav.Link>
-            </Nav>
+          
+          <NavWrapper>
+            <div>
+            <ul>
+              <li><a href="#overview">Overview</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#blog">Blog</a></li>
+            </ul>
             <OnlineIdentities/>
             <ColourPalette/>
-          </Navbar>
+            </div>
+          </NavWrapper>
+          
         )
     }
 }
 
-export default NavHeader;
+export default styled(NavHeader)``;
