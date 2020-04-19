435d7856359d27060804f142c838dd97bb44861d
davewd <davewd@me.com>
2020-04-19 22:11:21 +1000

    Add Location Component Template

---
+++ site/src/components/Locations.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const Locations = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Locations.propTypes = {
+    
+};
+
+export default Locations;
